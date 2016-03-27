8ce4986e3bd262f73da6952eb8591340d8753b79
DMor <Dmor574@knights.ucf.edu>
2016-03-27 17:22:48 -0400

    Added database connection example in dbcode folder

---
+++ dbcode/dbconnect.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "admin",
+  password: "everythingisawesome",
+  database: "activiti_data"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.query('SELECT * FROM employees',function(err,rows){
+  if(err) {
+	console.log("Query Error");
+	throw err;
+  }
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
