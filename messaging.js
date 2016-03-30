ce2a2f3562156c974af91b0e6af5add9a07be121
Jacob Wurm <jacob.wurm@knights.ucf.edu>
2016-03-30 02:09:41 -0400

    Fixed the merging....again

--- messaging.js
+++ messaging.js
@@ -2,10 +2,8 @@
 var express = require('express');
 var router = express.Router();
 var DBConnection = require('./node_modules/database/DBConnection');
-<<<<<<< HEAD
-=======
 var con = new DBConnection();
->>>>>>> 75ae2ae47d6406c043e47340753fe3e6d73d2e16
+
 
 router.post('/', function(req,res){
 	res.send('POST: Create a message');
@@ -25,10 +23,7 @@ router.delete('/', function(req,res) {
 router.get('/', function(req,res) {
 	//Needed to re-instantiate the con in order to reuse it
 	//Because otherwise, the reference is a dead connection after it's ended once
-<<<<<<< HEAD
 	var con = new DBConnection();
-=======
->>>>>>> 75ae2ae47d6406c043e47340753fe3e6d73d2e16
 	var query = "select * from users";	
 	con.sendQuery(query, queryResponse, res);
 	con.endConnection();
