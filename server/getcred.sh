77a13efda4568ec00b3d1f4dce21813c5e99fa50
Jacob Wurm <jacob.wurm@knights.ucf.edu>
2016-04-02 12:54:27 -0400

    Modified the start and credential scripts

--- server/getcred.sh
+++ server/getcred.sh
@@ -2,9 +2,11 @@
 
 echo "Enter activiti.servebeer.com ssh username: "
 read NAME
-
-scp "$NAME"@activiti.servebeer.com:/home/central/credentials.tar.gz ./
-tar xvf credentials.tar.gz
+echo "Fetching credentials"
+scp "$NAME"@activiti.servebeer.com:/home/central/credentials.tar.gz ./ &>/dev/null
+echo "Extracting credentials"
+tar xvf credentials.tar.gz &>/dev/null
 mv connection.json $PWD/node_modules/database/
+echo "Removing package"
 rm -f credentials.tar.gz
-
+echo "Update complete!"
--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 #Adding watch parameters so it will watch more than what is in the root directory
+
+
+echo "Starting server"
 nodemon --watch node_modules/database --watch ./ api.js
