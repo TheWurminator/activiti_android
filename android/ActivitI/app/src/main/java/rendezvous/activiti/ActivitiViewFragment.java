bd21b31368a2fbb01199a4bb6ba0fbcdb01abaab
James Yoder <jamesdeanyoder@gmail.com>
2016-04-07 19:32:13 -0400

    Restructured activities to be fragments.

---
+++ android/ActivitI/app/src/main/java/rendezvous/activiti/ActivitiViewFragment.java
@@ -0,0 +1,21 @@
+package rendezvous.activiti;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ActivitiViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_activiti_view, container, false);
+        return view;
+    }
+
+}
--- android/ActivitI/app/src/main/java/rendezvous/activiti/MainMenuActivity.java
+++ android/ActivitI/app/src/main/java/rendezvous/activiti/MainMenuActivity.java
@@ -1,11 +1,10 @@
 package rendezvous.activiti;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -18,7 +17,26 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     public void viewProfile(View view) {
-        Intent intent = new Intent(this, ProfileViewActivity.class);
-        startActivity(intent);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ProfileViewFragment profileViewFragment = new ProfileViewFragment();
+        fragmentTransaction.replace(R.id.fragmentContainer, profileViewFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void editProfile(View view) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        EditProfileFragment editProfileFragment = new EditProfileFragment();
+        fragmentTransaction.replace(R.id.fragmentContainer, editProfileFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void viewActiviti(View view) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ActivitiViewFragment activitiViewFragment = new ActivitiViewFragment();
+        fragmentTransaction.replace(R.id.fragmentContainer, activitiViewFragment);
+        fragmentTransaction.commit();
     }
 }
