5e0882e25cafcd6e33f936f6e5e23d35a4c63b48
DMor <Dmor574@knights.ucf.edu>
2016-04-08 16:07:37 -0400

    Renamed api.js to fix merge conflict

---
+++ server/api.js
@@ -0,0 +1,15 @@
+//Instantiate Modules
+var app = require('express')();
+var fs = require('fs'); //Access ssl key and cert
+var https = require('https'); //Enable ssl (secure connection)
+var port = process.env.PORT || 8081; //Configure port for server to listen
+var router = require('./router')(app); //Configure routes
+
+//Create server using secure ssl connection on specified port
+https.createServer({
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem')
+}, app).listen(port);
+
+//Debug Message
+console.log('Server Running on Port: ' + port);
