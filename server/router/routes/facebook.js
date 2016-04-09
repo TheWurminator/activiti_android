73f01160127e42f630d268c67165cdeaddd33dea
DMor <Dmor574@knights.ucf.edu>
2016-04-08 20:42:09 -0400

    Separated user permissions for facebook into separate file, Reorganized facebook.js

--- server/router/routes/facebook.js
+++ server/router/routes/facebook.js
@@ -1,17 +1,13 @@
 //Init
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser'); //Need this to parse the body of an http request
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
 var router = express.Router();
 var facebookUser = require('../../classes/fbuser.js');
 var fbauth = require('../../fbauth.json');
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(bodyParser.json()); //Need to this to be able to parse http requests for JSON
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-app.use(passport.session());
+var user_permissions = require('./fb_permissions/user_permissions.json');
+
 
 //This is called when the user actually logs in
 passport.use(new Strategy(fbauth, function(accessToken, refreshToken, profile, cb) {
@@ -28,14 +24,13 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 //Redirect User to facebook
-router.get('/', 
-	 passport.authenticate('facebook', { authType: 'rerequest', scope: ['user_birthday', 'user_about_me', 'public_profile'] ,failureRedirect: '/login/facebook' }));
+router.get('/', passport.authenticate('facebook', user_permissions));
 
 //Return from facebook after authentication
-router.get('/return', passport.authenticate('facebook', { authType: 'rerequest', scope: ['user_birthday', 'user_about_me', 'public_profile'] ,failureRedirect: '/login' },
-	  function(req, res) {
-	    res.redirect('/');
-	  })
+router.get('/return', passport.authenticate('facebook', user_permissions,
+	function(req, res) {
+		res.send('Facebook Redirected.');
+	})
 );
 
 // Exporting the functionality of the router to the calling module
