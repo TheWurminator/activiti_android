395534d5397f02d501741eb5827f77c932ac5275
DMor <Dmor574@knights.ucf.edu>
2016-04-09 18:01:35 -0400

    Authentication protect routes added to index file

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,21 @@
 //Initialize and Export Routes
 module.exports = function (app) {
+	//Verify valid token
+	var auth = function IsAuthenticated(req,res,next){
+		var tokenChecker = require('../node_modules/token-auth-check/tokenCheck');
+		var token = req.get('token');
+
+		if(tokenChecker.checkToken(token)) 
+        	next();
+		else 
+			res.send(403);
+	};
+		
 	//Application Routes - From Web-URL redirect to specified file for processing
-	app.use('/api/user', require('./routes/user'));
-	app.use('/api/activiti', require('./routes/activiti')); 
-	app.use('/api/search', require('./routes/search')); 
-	app.use('/api/messaging', require('./routes/messaging'));
+	app.use('/api/user', auth, require('./routes/user'));
+	app.use('/api/activiti', auth, require('./routes/activiti')); 
+	app.use('/api/search', auth, require('./routes/search')); 
+	app.use('/api/messaging', auth, require('./routes/messaging'));
 
 	//Facebook Routes - Used to authenticate with facebook server
 	app.use('/api/login/facebook', require('./routes/facebook'));
