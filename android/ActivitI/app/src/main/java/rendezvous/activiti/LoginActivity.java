b033d467c8ca5a33c9cc162701a6ea0a5cee1fdc
Amon Al-Khatib <amon.alkhatib@gmail.com>
2016-04-18 21:54:58 -0400

    Updated Login View
    Added new color theme and added logo + brand name to login screen.

--- android/ActivitI/app/src/main/java/rendezvous/activiti/LoginActivity.java
+++ android/ActivitI/app/src/main/java/rendezvous/activiti/LoginActivity.java
@@ -15,6 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
