214ac3f3fd89909e34ff0595bac2ea3359f684a7
Jacob Wurm <jacob.wurm@knights.ucf.edu>
2016-04-21 02:04:50 -0400

    HTTPS is back

--- server/app.js
+++ server/app.js
@@ -5,12 +5,12 @@ var https = require('https'); //Enable ssl (secure connection)
 var port = process.env.PORT || 8081; //Configure port for server to listen
 var router = require('./router')(app); //Configure routes
 
-//Create server using secure ssl connection on specified port
-// https.createServer({
-// 	key: fs.readFileSync('key.pem'), //Private Key
-// 	cert: fs.readFileSync('cert.pem') //Public certificate
-// }, app).listen(port);
+// Create server using secure ssl connection on specified port
+https.createServer({
+	key: fs.readFileSync('key.pem'), //Private Key
+	cert: fs.readFileSync('cert.pem') //Public certificate
+}, app).listen(port);
 
-app.listen(port);
+// app.listen(port);
 //Debug Message
 console.log('Server Running on Port: ' + port);
