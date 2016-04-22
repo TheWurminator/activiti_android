fafb782ac09002f1920c016967ced2afb8547cfa
James Yoder <jamesdeanyoder@gmail.com>
2016-04-21 22:34:47 -0400

    Started working on facebook login

--- ActivitI/app/src/main/java/rendezvous/activiti/LoginActivity.java
+++ ActivitI/app/src/main/java/rendezvous/activiti/LoginActivity.java
@@ -4,10 +4,13 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,32 +20,34 @@ import com.android.volley.toolbox.StringRequest;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginFragment;
 
-public class LoginActivity extends AppCompatActivity {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
-   // private CallbackManager callbackManager;
-    private String responseString = "Test";
-    private WebLoginFragment webLoginFragment = new WebLoginFragment();
-    private LoginFragment loginFragment = new LoginFragment();
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(MyApplication.getAppContext());
         setContentView(R.layout.content_login_page);
-        //navigate(LoginFragment);
-        //sendRequest();
     }
 
     public void loginServer(View view){
-        //Intent i = new Intent(Intent.ACTION_VIEW,
-        //        Uri.parse("https://activiti.servebeer.com:8081/api/login/facebook"));
-
-        // Starts Implicit Activity
-        //startActivity(i);
         setContentView(R.layout.content_web_login);
         WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new SSLTolerantWebView());
+        //trustEveryone();
         myWebView.loadUrl("https://activiti.servebeer.com:8081/api/login/facebook");
-        //myWebView.loadUrl("http://www.google.com");
     }
 
     public void navigate(Fragment fragment) {
@@ -53,43 +58,47 @@ public class LoginActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    /*public void loginClick(View view) {
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "user_about_me", "user_birthday"));
-    }*/
-
-    public void sendRequest(){
-        VolleySingleton volleySing = VolleySingleton.getInstance();
-        RequestQueue queue = VolleySingleton.getInstance().getRequestQueue();
-
-        StringRequest testRequest = new StringRequest(Request.Method.GET, "https://www.google.com/", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                //setResponseString(response);
-                //TextView testView = (TextView) findViewById(R.id.testView);
-                //testView.setText(getResponseString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error){
-                System.out.println("Something went wrong somewhere.");
-                error.printStackTrace();
-            }
-        });
-
-        queue.add(testRequest);
-    }
-
-    public void setResponseString(String val){
-        responseString = val;
-    }
-
-    public String getResponseString(){
-        return responseString;
-    }
-
     public void getmein(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }});
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager(){
+                public void checkClientTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public void checkServerTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }}}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                    context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
+
+    private class SSLTolerantWebView extends WebViewClient {
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            URL aURL = null;
+            try {
+                aURL = new URL(url);
+                URLConnection conn = aURL.openConnection();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
